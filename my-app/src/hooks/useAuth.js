9ba3f7db06d06c3e17998ee6225cd56d877fe5c9
Manh Duc <ducmanh200555@gmail.com>
2026-04-16 20:21:42 +0700

    logout

--- my-app/src/hooks/useAuth.js
+++ my-app/src/hooks/useAuth.js
@@ -1,45 +1,23 @@
 import { useState } from "react";
-import { login, logout } from "../services/authService";
-import { validateEmail, validatePassword } from "../utils/validator";
-
-export function useAuth() {
+import { validateEmail, validatePassword } from "../utils/validator.js";
+import { login } from "../services/authServices.js";
+export const useAuth = () => {
   const [userId, setUserId] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [state, setState] = useState(null);
   const [error, setError] = useState("");
-
   const handleLogin = async ({ email, password }) => {
+    setState("pending");
     setError("");
-
     try {
       validateEmail(email);
       validatePassword(password);
-      setLoading(true);
-
-      const userId = await login({ email, password });
-
-      // backend trả về { userId }
-      setUserId(userId);
+      const id = await login({ email, password });
+      setUserId(id);
+      setState("success");
     } catch (err) {
       setError(err.message);
-    } finally {
-      setLoading(false);
+      setState("fail");
     }
   };
-
-  const handleLogout = async () => {
-    try {
-      await logout(); // gọi backend để clear cookie
-      setUserId(null);
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
-  return {
-    userId,
-    loading,
-    error,
-    handleLogin,
-    handleLogout,
-  };
-}
+  const handleLogout = async () => {};
+};
--- my-app/src/services/authServices.js
+++ my-app/src/services/authServices.js
@@ -1,19 +1,35 @@
-const URL = "http://localhost:3000/student-portal/auth";
+const URL = "/api/student-portal/auth";
+
 export const login = async ({ email, password }) => {
   const response = await fetch(`${URL}/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      email,
-      password,
-    }),
+    credentials: "include",
+    body: JSON.stringify({ email, password }),
   });
+
   const data = await response.json();
+
   if (!response.ok) {
     throw new Error(data.message || "Login failed");
   }
-  console.log(data.message);
+
   return data.userId;
+};
+
+export const logout = async () => {
+  const response = await fetch(`${URL}/logout`, {
+    method: "POST",
+    credentials: "include",
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Logout failed");
+  }
+
+  return data.message;
 };
