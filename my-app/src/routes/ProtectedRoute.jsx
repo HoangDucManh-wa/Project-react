8f38f7fb99fb7b526b13f4fe8c08670b253048b2
Manh Duc <ducmanh200555@gmail.com>
2026-04-21 09:23:17 +0700

    upgrade ProtectedRoute.jsx

--- my-app/src/routes/ProtectedRoute.jsx
+++ my-app/src/routes/ProtectedRoute.jsx
@@ -1,18 +1,15 @@
 import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext.jsx";
-
-const ProtectedRoute = ({ children }) => {
+export const ProtectedRoute = ({ children }) => {
   const { user, state } = useAuthContext();
-
-  if (state) {
-    return <div>{state}</div>;
+  if (state === "pending") {
+    return <div>pending...</div>;
   }
-
+  if (state === "error") {
+    return <div>Có lỗi xác thực</div>;
+  }
   if (!user) {
     return <Navigate to="/login" replace />;
   }
-
   return children;
 };
-
-export default ProtectedRoute;
